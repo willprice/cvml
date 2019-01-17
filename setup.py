77495406fc7b776c35f4f5a0fb1e6c563469164b
Will Price <will.price94@gmail.com>
2019-01-17 18:36:03 +0000

    Rename to cvml as libml isnt free on pypi

--- setup.py
+++ setup.py
@@ -2,22 +2,23 @@
 
 from setuptools import find_packages, setup
 
-import ml
+import cvml
 setup(
-        name='ml',
-        description='Machine Learning helpers',
-        version=ml.__version__,
-        author='Will Price',
-        author_email='will.price94@gmail.com',
-        url='http://github.com/willprice/libml',
-        packages=find_packages(),
-        classifiers=[
-            # How mature is this project? Common values are
-            #   3 - Alpha
-            #   4 - Beta
-            #   5 - Production/Stable
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Programming Language :: Python :: 3.6',
-        ],
+    name='cvml',
+    description='Computer vision machine learning tools',
+    version=cvml.__version__,
+    author='Will Price',
+    author_email='will.price94@gmail.com',
+    url='http://github.com/willprice/libml',
+    packages=find_packages(),
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
