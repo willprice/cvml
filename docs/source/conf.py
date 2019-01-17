5113ddb789de5effe91528bf7ce5b121c59e2df2
Will Price <will.price94@gmail.com>
2019-01-17 18:46:23 +0000

    Rename libml to cvml in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'libml'
+project = 'cvml'
 copyright = '2018, Will Price'
 author = 'Will Price'
 
@@ -107,7 +107,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'libmldoc'
+htmlhelp_basename = 'cvmldoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -134,7 +134,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'libml.tex', 'libml Documentation',
+    (master_doc, 'cvml.tex', 'cvml Documentation',
      'Will Price', 'manual'),
 ]
 
@@ -144,7 +144,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'libml', 'libml Documentation',
+    (master_doc, 'cvml', 'cvml Documentation',
      [author], 1)
 ]
 
@@ -155,8 +155,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'libml', 'libml Documentation',
-     author, 'libml', 'One line description of project.',
+    (master_doc, 'cvml', 'cvml Documentation',
+     author, 'cvml', 'One line description of project.',
      'Miscellaneous'),
 ]
 
